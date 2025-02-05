e2800663a76de3aa04d6422419d55657b81f5b77
CBurg-College <cburgmicro@outlook.com>
2025-02-05 22:57:53 +0100

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -3,15 +3,235 @@
 //% block.loc.nl="Kweekbakje"
 namespace CBreedingBox {
 
-    let PIN_LIGHT = DigitalPin.P0
-    let PIN_SDA = DigitalPin.P0
-    let PIN_SCL = DigitalPin.P0
+    let PIN_LIGHT = DigitalPin.P1
+
 
     let MOISTURE : number = 0
     let HUMIDITY : number = 0
     let TEMPERATURE : number = 0
     let LIGHT : number = 0
     let PRESSURE : number = 0
+
+    /////////////
+    // ESP8266 //
+    /////////////
+
+    /*
+    The ESP8266 code is taken from the ElecFreaks 'ESP8266.ts' library:
+    https://github.com/elecfreaks/pxt-esp8266iot/blob/master/ESP8266.ts
+    (MIT-license)
+    */
+
+    enum Cmd {
+        None,
+        ConnectWifi,
+        ConnectThingSpeak,
+        ConnectSmartIot,
+        InitSmartIot,
+        UploadSmartIot,
+        DisconnectSmartIot,
+        ConnectMqtt,
+    }
+
+    const EspEventSource = 3000
+    const EspEventValue = {
+        None: Cmd.None,
+        ConnectWifi: Cmd.ConnectWifi,
+        ConnectThingSpeak: Cmd.ConnectThingSpeak,
+        ConnectSmartIot: Cmd.ConnectSmartIot,
+        InitSmartIot: Cmd.InitSmartIot,
+        UploadSmartIot: Cmd.UploadSmartIot,
+        DisconnectSmartIot: Cmd.DisconnectSmartIot,
+        ConnectMqtt: Cmd.ConnectMqtt,
+        PostIFTTT: 255
+    }
+
+    const THINGSPEAK_HOST = "api.thingspeak.com"
+    const THINGSPEAK_PORT = "80"
+
+    let wifi_connected: boolean = false
+    let thingspeak_connected: boolean = false
+    let thingSpeakDatatemp = ""
+
+    let serialCnt = 0
+    let recvString = ""
+    let scanWIFIAPFlag = 0
+    let currentCmd: Cmd = Cmd.None
+    let tsSendStr = ""
+
+    // write AT command with CR+LF ending
+    function sendAT(command: string, wait: number = 0) {
+        serial.writeString(`${command}\u000D\u000A`)
+        basic.pause(wait)
+    }
+
+    function restEsp8266() {
+        sendAT("AT+RESTORE", 1000) // restore to factory settings
+        sendAT("AT+RST", 1000) // rest
+        serial.readString()
+        sendAT("AT+CWMODE=1", 500) // set to STA mode
+        sendAT("AT+SYSTIMESTAMP=1634953609130", 100) // Set local timestamp.
+        sendAT(`AT+CIPSNTPCFG=1,8,"ntp1.aliyun.com","0.pool.ntp.org","time.google.com"`, 100)
+        basic.pause(3000)
+    }
+
+    function initWIFI() {
+        serial.redirect(SerialPin.P12, SerialPin.P8, BaudRate.BaudRate115200)
+        basic.pause(100)
+        serial.setTxBufferSize(128)
+        serial.setRxBufferSize(128)
+        restEsp8266()
+    }
+
+    function scanWIFIAP(ssid: string) {
+        let scanflag = 0
+        let mscnt = 0
+        recvString = " "
+        sendAT(`AT+CWLAPOPT=1,2,-100,255`)
+        sendAT(`AT+CWLAP`)
+        while (!(scanflag)) {
+            recvString = recvString + serial.readString()
+            basic.pause(1)
+            mscnt += 1
+            if (mscnt >= 3000) {
+                scanWIFIAPFlag = 0
+                break
+            }
+            if (recvString.includes("+CWLAP:(")) {
+                mscnt = 0
+                recvString = recvString.slice(recvString.indexOf("+CWLAP:("))
+                scanflag = 1
+                while (1) {
+                    recvString += serial.readString()
+                    basic.pause(1)
+                    mscnt += 1
+                    // OLED.clear()
+                    // OLED.writeStringNewLine(_recvString)
+                    if (recvString.includes("OK") || mscnt >= 3000) {
+                        if (mscnt >= 3000) {
+                            scanWIFIAPFlag = 0
+                        } else if (recvString.includes(ssid)) {
+                            scanWIFIAPFlag = 1
+                        } else {
+                            scanWIFIAPFlag = 0
+                        }
+                        break
+                    }
+                }
+            }
+        }
+        recvString = " "
+    }
+
+    function connectWIFI(ssid: string, pw: string) {
+        while (1) {
+            scanWIFIAP(ssid)
+            if (scanWIFIAPFlag) {
+                currentCmd = Cmd.ConnectWifi
+                sendAT(`AT+CWJAP="${ssid}","${pw}"`) // connect to Wifi router
+                control.waitForEvent(EspEventSource, EspEventValue.ConnectWifi)
+                while (!wifi_connected) {
+                    restEsp8266()
+                    sendAT(`AT+CWJAP="${ssid}","${pw}"`)
+                    control.waitForEvent(EspEventSource, EspEventValue.ConnectWifi)
+                }
+                break
+            } else {
+                restEsp8266()
+                currentCmd = Cmd.ConnectWifi
+                sendAT(`AT+CWJAP="${ssid}","${pw}"`)
+                control.waitForEvent(EspEventSource, EspEventValue.ConnectWifi)
+                if (wifi_connected) {
+                    break
+                }
+            }
+        }
+        thingspeak_connected = true
+    }
+
+    serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+        recvString += serial.readString()
+        pause(1)
+        serialCnt += 1
+
+        if (recvString.includes("Congratu")) {
+            recvString = ""
+            control.raiseEvent(EspEventSource, EspEventValue.PostIFTTT)
+        }
+
+        switch (currentCmd) {
+            case Cmd.ConnectWifi:
+                if (recvString.includes("AT+CWJAP")) {
+                    recvString = recvString.slice(recvString.indexOf("AT+CWJAP"))
+                    if (recvString.includes("WIFI GOT IP")) {
+                        wifi_connected = true
+                        recvString = ""
+                        control.raiseEvent(EspEventSource, EspEventValue.ConnectWifi)
+                    } else if (recvString.includes("ERROR")) {
+                        wifi_connected = false
+                        recvString = ""
+                        control.raiseEvent(EspEventSource, EspEventValue.ConnectWifi)
+                    }
+                }
+                break
+            case Cmd.ConnectThingSpeak:
+                if (recvString.includes(THINGSPEAK_HOST)) {
+                    recvString = recvString.slice(recvString.indexOf(THINGSPEAK_HOST))
+                    if (recvString.includes("CONNECT")) {
+                        thingspeak_connected = true
+                        recvString = ""
+                        control.raiseEvent(EspEventSource, EspEventValue.ConnectThingSpeak)
+                    } else if (recvString.includes("ERROR")) {
+                        thingspeak_connected = false
+                        recvString = ""
+                        control.raiseEvent(EspEventSource, EspEventValue.ConnectThingSpeak)
+                    }
+                } else if (recvString.includes("WIFI GOT IP")) {
+                    thingspeak_connected = false
+                    recvString = ""
+                    control.raiseEvent(EspEventSource, EspEventValue.ConnectThingSpeak)
+                }
+                break
+        }
+    })
+
+    function setData(write_api_key: string, n1: number = 0, n2: number = 0, n3: number = 0, n4: number = 0, n5: number = 0, n6: number = 0, n7: number = 0, n8: number = 0) {
+        tsSendStr = "AT+HTTPCLIENT=2,0,\"http://api.thingspeak.com/update?api_key="
+            + write_api_key
+            + "&field1="
+            + n1
+            + "&field2="
+            + n2
+            + "&field3="
+            + n3
+            + "&field4="
+            + n4
+            + "&field5="
+            + n5
+            + "&field6="
+            + n6
+            + "&field7="
+            + n7
+            + "&field8="
+            + n8
+            + "\",,,1"
+    }
+
+    function uploadData() {
+        let mscnt = 0
+        //sendAT(`AT+CIPSEND=${tsSendStr.length + 2}`, 300)
+        sendAT(tsSendStr, 100) // upload data
+        while (1) {
+            recvString += serial.readString()
+            basic.pause(1)
+            mscnt += 1
+            if (recvString.includes("OK") || mscnt >= 3000 || recvString.includes("ERROR")) {
+                break
+            }
+        }
+        recvString = " "
+        basic.pause(200)
+    }
 
     ////////////
     // BME280 //
@@ -208,4 +428,16 @@ namespace CBreedingBox {
         return TEMPERATURE
     }
 
+    //% block="connected to ThingSpeak"
+    //% block.loc.nl="verbonden met ThingSpeak"
+    export function thingSpeakConneced() : boolean {
+        return (thingspeak_connected && wifi_connected)
+    }
+
+    //% block="start via ssid %ssid and password %passw" using writekey %wkey and readkey %rkey"
+    //% block="start via ssid %ssid en wachtwoord %passw" met writekey %wkey and readkey %rkey"
+    export function connect(ssid: string, passw: string, wkey: string, rkey: string) {
+        initWIFI()
+        connectWIFI(ssid, passw)
+    }
 }
