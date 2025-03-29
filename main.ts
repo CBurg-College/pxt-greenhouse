a3efacb25360a6259029ed4928b39874c4c24f9c
CBurg-College <cburgmicro@outlook.com>
2025-03-29 20:15:45 +0100

    pxt.json, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -584,25 +584,8 @@ namespace ESP8266 {
 /*
 The neopixel code is copied from the Microsoft 'neopixel.ts' library:
 https://github.com/microsoft/pxt-neopixel
-https://github.com/microsoft/pxt-ws2812b
 (MIT-license)
 */
-
-namespace ws2812b {
-    //% shim=sendBufferAsm
-    export function sendBuffer(buf: Buffer, pin: DigitalPin) {
-    }
-
-    //% shim=setBufferMode
-    export function setBufferMode(pin: DigitalPin, mode: number) {
-
-    }
-
-    export const BUFFER_MODE_RGB = 1
-    export const BUFFER_MODE_RGBW = 2
-    export const BUFFER_MODE_RGB_RGB = 3
-    export const BUFFER_MODE_AP102 = 4
-}
 
 enum NeoPixelMode {
     RGB = 1,
