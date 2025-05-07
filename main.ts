d23bb2d384fd3ad435c8b0e44ec2d252dab70157
CBurg-College <cburgmicro@outlook.com>
2025-05-07 22:09:49 +0200

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -849,8 +849,89 @@ namespace DHT22 {
         dataPin = pin
     }
 
+    export enum DHT11Type {
+        //% block="temperature(℃)" enumval=0
+        DHT11_temperature_C,
+
+        //% block="temperature(℉)" enumval=1
+        DHT11_temperature_F,
+
+        //% block="humidity(0~100)" enumval=2
+        DHT11_humidity,
+    }
+
+    export function temperature(dht11type: DHT11Type, dht11pin: DigitalPin): number {
+
+        pins.digitalWritePin(dht11pin, 0)
+        basic.pause(18)
+        let i = pins.digitalReadPin(dht11pin)
+        pins.setPull(dht11pin, PinPullMode.PullUp);
+        switch (dht11type) {
+            case 0:
+                let dhtvalue1 = 0;
+                let dhtcounter1 = 0;
+                while (pins.digitalReadPin(dht11pin) == 1);
+                while (pins.digitalReadPin(dht11pin) == 0);
+                while (pins.digitalReadPin(dht11pin) == 1);
+                for (let i = 0; i <= 32 - 1; i++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    dhtcounter1 = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        dhtcounter1 += 1;
+                    }
+                    if (i > 15) {
+                        if (dhtcounter1 > 2) {
+                            dhtvalue1 = dhtvalue1 + (1 << (31 - i));
+                        }
+                    }
+                }
+                return ((dhtvalue1 & 0x0000ff00) >> 8);
+                break;
+            case 1:
+                while (pins.digitalReadPin(dht11pin) == 1);
+                while (pins.digitalReadPin(dht11pin) == 0);
+                while (pins.digitalReadPin(dht11pin) == 1);
+                let dhtvalue = 0;
+                let dhtcounter = 0;
+                for (let i = 0; i <= 32 - 1; i++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    dhtcounter = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        dhtcounter += 1;
+                    }
+                    if (i > 15) {
+                        if (dhtcounter > 2) {
+                            dhtvalue = dhtvalue + (1 << (31 - i));
+                        }
+                    }
+                }
+                return Math.round((((dhtvalue & 0x0000ff00) >> 8) * 9 / 5) + 32);
+                break;
+            case 2:
+                while (pins.digitalReadPin(dht11pin) == 1);
+                while (pins.digitalReadPin(dht11pin) == 0);
+                while (pins.digitalReadPin(dht11pin) == 1);
+
+                let value = 0;
+                let counter = 0;
+
+                for (let i = 0; i <= 8 - 1; i++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    counter = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        counter += 1;
+                    }
+                    if (counter > 3) {
+                        value = value + (1 << (7 - i));
+                    }
+                }
+                return value;
+            default:
+                return 0;
+        }
+    }
+
     export function measure() {
-        dataPin = DigitalPin.P14
 
         //initialize
         let checksum: number = 0
@@ -991,7 +1072,10 @@ namespace CBreedingBox {
         MOISTURE = Math.round(valueS)
         switch ( SENSOR) {
             case Sensor.Bme280: BME280.measure(); break;
-            case Sensor.Dht22:  DHT22.measure(); break;
+//            case Sensor.Dht22:  DHT22.measure(); break;
+            case Sensor.Dht22:
+                DHT22.TEMPERATURE = DHT22.temperature( DHT22.DHT11Type.DHT11_temperature_C,
+                    DigitalPin.P14)
         }
     }
 
